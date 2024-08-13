df70a8090a950f732771f27027b6b2469dcb4fda
Charles Bousseau <16641587+cbouss@users.noreply.github.com>
2024-08-13 09:18:40 -0400

    Update recipe/build.sh
    Co-authored-by: danpetry <dpetry@anaconda.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,7 +22,7 @@ function build() {
         -DCMAKE_INSTALL_LIBDIR=lib \
         -DHAVE_LIBZ=FALSE -DHAVE_LIBLZO2=FALSE \
         -DSNAPPY_BUILD_BENCHMARKS=OFF \
-	    -DCMAKE_CXX_STANDARD=17 \
+	-DCMAKE_CXX_STANDARD=17 \
         -DSNAPPY_ENABLE_RTTI=ON \
         $extra_args
 
